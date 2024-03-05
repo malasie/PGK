f6d2f14c220e888fbea85bb6d0b20bf940b0518a
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2024-03-05 11:36:35 +0100

    dodatkowe funkcje

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -31,16 +31,18 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 20);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 38);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(82, 20);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "zmień kierunek";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -50,14 +52,36 @@
             this.timer1.Interval = 30;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F);
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "resetuj prędkość";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -67,6 +91,7 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.Resize += new System.EventHandler(this.Form1_Resize_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +99,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,20 +18,21 @@ namespace WindowsFormsApp1
         {
             bmp = new Bitmap(Width+1000, Height+400);
             InitializeComponent();
+
             
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var g = this.CreateGraphics();
-
-            g.DrawEllipse(Pens.Black, 100, 100, 50, 50);
+            k_X *= -1;
+            k_Y *= -1;
             
 
 
 
         }
-
         int X = 0;
         int oldX = 0;
         bool P = true;
@@ -42,6 +44,7 @@ namespace WindowsFormsApp1
         int r = 50;
         int pr_X = 1;
         int pr_Y = 1;
+        int max_pr = 5;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -89,10 +92,14 @@ namespace WindowsFormsApp1
             
             var gb = Graphics.FromImage(bmp);
             var gb2 = Graphics.FromImage(bmp);
+            var gb3 = Graphics.FromImage(bmp);
             gb.FillRectangle(SystemBrushes.Control, 0, 0, Width, Height);
-            Rectangle rect = new Rectangle(400, 100, 200, 150);
-            gb.FillRectangle(new SolidBrush(Color.PowderBlue), rect);
+            Rectangle rect = new Rectangle(0,0, 250, 400);
+            gb3.TranslateTransform(800.0F, 300.0F);
+            gb3.RotateTransform(45.0F);
+            gb3.FillRectangle(new SolidBrush(Color.PaleGoldenrod), rect);
             gb.FillEllipse(new SolidBrush(Color.RosyBrown), 50, 350, 250, 180);
+            gb.FillRectangle(new SolidBrush(Color.PowderBlue), 400, 100, 200, 150);
 
             gb2.DrawEllipse(Pens.Blue, 200+X, 200+Y, r, r);
             oldX = X;
@@ -108,13 +115,27 @@ namespace WindowsFormsApp1
 
            
 
-            if (X <= -200 || X >= w-200-r)
+            if (X >= w-200-r)
             {
+                X = w - 200 - r;
                 k_X *= -1;
 
             }
-            if (Y <= -200 || Y >= h -200 - r)
+            if (X <= -200)
             {
+                X = -200;
+                k_X *= -1;
+
+            }
+            if (Y >= h -200 - r)
+            {
+                Y = h - 200 - r;
+                k_Y *= -1;
+
+            }
+            if (Y <= -200)
+            {
+                Y = -200;
                 k_Y *= -1;
 
             }
@@ -143,6 +164,7 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label1.Text = $"Predkość X: {pr_X}\nPredkość Y: {pr_Y}";
 
         }
 
@@ -165,7 +187,7 @@ namespace WindowsFormsApp1
             {
                 pr_Y -= 1;
             }
-            if (e.KeyCode == Keys.Up & pr_Y<5)
+            if (e.KeyCode == Keys.Up & pr_Y<max_pr)
             {
                 pr_Y += 1;
             }
@@ -174,12 +196,21 @@ namespace WindowsFormsApp1
             {
                 pr_X -= 1;
             }
-            if (e.KeyCode == Keys.Right & pr_X < 5)
+            if (e.KeyCode == Keys.Right & pr_X < max_pr)
             {
                 pr_X += 1;
             }
+
+            label1.Text = $"Predkość X: {pr_X}\nPredkość Y: {pr_Y}";
         }
 
-        
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            pr_X = 1;
+            pr_Y = 1;
+            label1.Text = $"Predkość X: {pr_X}\nPredkość Y: {pr_Y}";
+        }
+
+     
     }
 }
