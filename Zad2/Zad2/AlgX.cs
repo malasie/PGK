ae1339f0cd7a4ef4666ad82e7e993232c370a20e
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2024-03-07 14:09:05 +0100

    Zaj0703

---
+++ Zad2/Zad2/AlgX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Zad2
+{
+    internal class AlgX : IAlg
+    {
+        public void calcNextStep(List<Ball> balls)
+        {
+            foreach (Ball ball in balls)
+            {
+                ball.X = ball.Vx + ball.X;
+            }
+        }
+    }
+}
---
+++ Zad2/Zad2/AlgY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Zad2
+{
+    internal class AlgY : IAlg
+    {
+        public void calcNextStep(List<Ball> balls)
+        {
+            foreach (Ball ball in balls)
+            {
+                ball.Y = ball.Vy + ball.Y;
+            }
+        }
+
+    }
+}
