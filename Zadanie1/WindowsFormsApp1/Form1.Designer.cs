3e894c679bbf281efad0fe4bf6821de8117cf40f
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2024-04-28 19:02:05 +0200

    zadanie 7

---
+++ Zadanie1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "zmień kierunek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F);
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "resetuj prędkość";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Resize += new System.EventHandler(this.Form1_Resize_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
--- Zadanie7/Test/Zadanie7.cpp
+++ Zadanie7/Test/Zadanie7.cpp
@@ -5,6 +5,7 @@
 #include <tchar.h>
 
 
+
 int rot_Z = 0;
 int rot_Y = 0;
 int rot_X = 0;
@@ -19,6 +20,9 @@ GLuint t3;
 GLuint t4;
 GLuint t5;
 GLuint t6;
+GLuint tr;
+
+bool cube = true;
 
 
 bool LoadBitmapTex(LPCWSTR szFileName, GLuint& texid)
@@ -30,7 +34,7 @@ bool LoadBitmapTex(LPCWSTR szFileName, GLuint& texid)
         LR_LOADFROMFILE);
     if (!hBMP)
     {
-        printf("false hBMP");
+        printf("false hBMP ");
         return FALSE;
     }
     //pobierz informacje o obiekcie bitmapy, zapisane dane do struktury
@@ -48,19 +52,66 @@ bool LoadBitmapTex(LPCWSTR szFileName, GLuint& texid)
     if (BMP.bmBitsPixel != 24)//musi byc bitmapa 24bit per pixel
     {
         DeleteObject(hBMP);
-        printf("false pixels");
+        printf("false pixels ");
         return FALSE;
     }
     //Dolacz bitmape do tekstury
     glTexImage2D(GL_TEXTURE_2D, 0, 3, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
     //usun bitmape z pamieci
     DeleteObject(hBMP);
-    printf("true");
     return TRUE;
 }
 
 
+void pokazSciane() 
+{
+    srand(time(NULL));
+    int random = rand() % 6;
 
+    if (random == 1)
+    {
+        rot_X = 0;
+        rot_Y = 180;
+        rot_Z = 0;
+    }
+    else if (random == 2)
+    {
+        rot_X = 0;
+        rot_Y = 90;
+        rot_Z = 270;
+
+    }
+    else if (random == 3)
+    {
+        rot_X = 90;
+        rot_Y = 0;
+        rot_Z = 270;
+
+    }
+    else if (random == 4)
+    {
+        rot_X = 270;
+        rot_Y = 0;
+        rot_Z = 90;
+
+    }
+    else if (random == 5)
+    {
+        rot_X = 0;
+        rot_Y = 270;
+        rot_Z = 270;
+
+    }
+    else
+    {
+        rot_X = 0;
+        rot_Y = 0;
+        rot_Z = 0;
+    }
+
+    //printf("sciana: %i,   X: %i, Y: %i, Z: %i\n", random, rot_X, rot_Y, rot_Z);
+
+}
 
 
 void funkcjaRenderujaca()
@@ -77,75 +128,97 @@ void funkcjaRenderujaca()
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_TEXTURE_2D);
 
-    
+
+    if (cube) {
+        // Sześcian
+        glPushMatrix();
+        glRotatef(rot_Z, 0, 0, 1);
+        glRotatef(rot_X, 0, 1, 0);
+        glRotatef(rot_Y, 1, 0, 0);
+        glEnd();
 
 
-// Sześcian
-    glPushMatrix();
-    glRotatef(rot_Z, 0, 0, 1);
-    glRotatef(rot_Y, 0, 1, 0);
-    glRotatef(rot_X, 1, 0, 0);
-    glEnd();
+        glBindTexture(GL_TEXTURE_2D, t6);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0); glVertex3f(-0.5f, -0.5f, -0.5f);
+        glTexCoord2f(1, 0); glVertex3f(0.5f, -0.5f, -0.5f);
+        glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, -0.5f);
+        glTexCoord2f(0, 1); glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    
+        glBindTexture(GL_TEXTURE_2D, t1);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f(-0.5f, -0.5f, 0.5f);
+        glTexCoord2f(1, 1); glVertex3f(0.5f, -0.5f, 0.5f);
+        glTexCoord2f(1, 0); glVertex3f(0.5f, 0.5f, 0.5f);
+        glTexCoord2f(0, 0); glVertex3f(-0.5f, 0.5f, 0.5f);
+        glEnd();
+
+        glBindTexture(GL_TEXTURE_2D, t3);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0); glVertex3f(0.5f, -0.5f, 0.5f);
+        glTexCoord2f(1, 0); glVertex3f(0.5f, 0.5f, 0.5f);
+        glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, -0.5f);
+        glTexCoord2f(0, 1); glVertex3f(0.5f, -0.5f, -0.5f);
+        glEnd();
+
+        glBindTexture(GL_TEXTURE_2D, t4);
+        glBegin(GL_QUADS);
+        glTexCoord2f(1, 0); glVertex3f(-0.5f, -0.5f, 0.5f);
+        glTexCoord2f(0, 0); glVertex3f(-0.5f, 0.5f, 0.5f);
+        glTexCoord2f(0, 1); glVertex3f(-0.5f, 0.5f, -0.5f);
+        glTexCoord2f(1, 1); glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
+
+        glBindTexture(GL_TEXTURE_2D, t5);
+        glBegin(GL_QUADS);
+        glTexCoord2f(1, 1); glVertex3f(-0.5f, 0.5f, 0.5f);
+        glTexCoord2f(0, 1); glVertex3f(-0.5f, 0.5f, -0.5f);
+        glTexCoord2f(0, 0); glVertex3f(0.5f, 0.5f, -0.5f);
+        glTexCoord2f(1, 0); glVertex3f(0.5f, 0.5f, 0.5f);
+        glEnd();
+
+        glBindTexture(GL_TEXTURE_2D, t2);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex3f(-0.5f, -0.5f, 0.5f);
+        glTexCoord2f(0, 0); glVertex3f(0.5f, -0.5f, 0.5f);
+        glTexCoord2f(1, 0); glVertex3f(0.5f, -0.5f, -0.5f);
+        glTexCoord2f(1, 1); glVertex3f(-0.5f, -0.5f, -0.5f);
+
+        glEnd();
+
+        glPopMatrix();
+
+    }
+
+    else 
+    {
 
 
+        glPushMatrix();
+             
+        glBindTexture(GL_TEXTURE_2D, tr);
+        glBegin(GL_TRIANGLES);
+        glTexCoord2f(1.27, 1.767); glVertex3f(-0.8f, 0.8f, -0.2f);
+        glTexCoord2f(1.27, 1.204); glVertex3f(-0.8f, -0.81f, -0.2f);
+        glTexCoord2f(1.815, 1.204); glVertex3f(0.83f, -0.81f, -0.2f);
+        glEnd();
+
+        glPopMatrix();
+
+        glBindTexture(GL_TEXTURE_2D, t2);
+        glBegin(GL_QUADS);
+        glVertex3f(1, 1, 0.5f);
+        glVertex3f(1, -1, 0.5f);
+        glVertex3f(-1, -1, 0.5f);
+        glVertex3f(-1, 1, 0.5f);
+        glEnd();
+
+    }
+
+   
 
 
-
-    glBindTexture(GL_TEXTURE_2D, t1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-0.5f, -0.5f, -0.5f);
-    glTexCoord2f(1, 0); glVertex3f(0.5f, -0.5f, -0.5f);
-    glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, -0.5f);
-    glTexCoord2f(0, 1); glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
-
-    glBindTexture(GL_TEXTURE_2D, t2);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-0.5f, -0.5f, 0.5f);
-    glTexCoord2f(0, 1); glVertex3f(0.5f, -0.5f, 0.5f);
-    glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, 0.5f);
-    glTexCoord2f(1, 0); glVertex3f(-0.5f, 0.5f, 0.5f);
-    glEnd();
-
-    glBindTexture(GL_TEXTURE_2D, t3);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(0.5f, -0.5f, 0.5f);
-    glTexCoord2f(1, 0); glVertex3f(0.5f, 0.5f, 0.5f);
-    glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, -0.5f);
-    glTexCoord2f(0, 1); glVertex3f(0.5f, -0.5f, -0.5f);
-    glEnd();
-
-    glBindTexture(GL_TEXTURE_2D, t4);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-0.5f, -0.5f, 0.5f);
-    glTexCoord2f(1, 0); glVertex3f(-0.5f, 0.5f, 0.5f);
-    glTexCoord2f(1, 1); glVertex3f(-0.5f, 0.5f, -0.5f);
-    glTexCoord2f(0, 1); glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
-
-    glBindTexture(GL_TEXTURE_2D, t5);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 1); glVertex3f(-0.5f, -0.5f, 0.5f);
-    glTexCoord2f(0, 0); glVertex3f(-0.5f, 0.5f, -0.5f);
-    glTexCoord2f(1, 0); glVertex3f(0.5f, 0.5f, -0.5f);
-    glTexCoord2f(1, 1); glVertex3f(0.5f, 0.5f, 0.5f);
-    glEnd();
-
-    glBindTexture(GL_TEXTURE_2D, t6);
-    glBegin(GL_QUADS);
-    glTexCoord2f(1, 0); glVertex3f(-0.5f, -0.5f, 0.5f);
-    glTexCoord2f(0, 0); glVertex3f(0.5f, -0.5f, 0.5f);
-    glTexCoord2f(0, 1); glVertex3f(0.5f, -0.5f, -0.5f);
-    glTexCoord2f(1, 1); glVertex3f(-0.5f, -0.5f, -0.5f);
-
-    glEnd();
-
-    glPopMatrix();
-
-    
-    
 
 
     //aktualizacja ekranu poprzez zamiane bufforów
@@ -159,6 +232,15 @@ void funkcjaKlawiatury(unsigned char key, int x, int y)
     case 27: // ESCAPE key
         exit(0);
         break;
+
+    case 't':
+        cube = !cube;
+        if (cube) {
+            pokazSciane();
+        }
+        glutPostRedisplay();
+        break;
+
     case 'z':
         //wcisnieto klawisz z
         rot_Z++;//zwieksz kat obrotu
@@ -166,12 +248,8 @@ void funkcjaKlawiatury(unsigned char key, int x, int y)
         {
             rot_Z = 0;
         }
-    case 'y':
-        rot_Y++;
-        if (rot_Y > 360)
-        {
-            rot_Y = 0;
-        }
+        glutPostRedisplay();//wymuszenie przerysowania sceny
+        break;
 
     case 'x':
         rot_X++;
@@ -179,13 +257,19 @@ void funkcjaKlawiatury(unsigned char key, int x, int y)
         {
             rot_X = 0;
         }
+        glutPostRedisplay();//wymuszenie przerysowania sceny
+        break;
 
-
-
+    case 'y':
+        rot_Y++;
+        if (rot_Y > 360)
+        {
+            rot_Y = 0;
+        }
         glutPostRedisplay();//wymuszenie przerysowania sceny
         break;
     }
-    
+
 }
 
 
@@ -221,19 +305,22 @@ int main(int argc, char* args[])
     glutPassiveMotionFunc(fM2);
     glutMouseFunc(fM3);
 
+    pokazSciane();
     // TEKSTURY
-    
+
     LoadBitmapTex(TEXT("bitmapy\\1.bmp"), t1);
-    
+
     LoadBitmapTex(TEXT("bitmapy\\2.bmp"), t2);
 
     LoadBitmapTex(TEXT("bitmapy\\3.bmp"), t3);
 
-    LoadBitmapTex(TEXT("bitmapy\\4.bmp"), t1);
+    LoadBitmapTex(TEXT("bitmapy\\4.bmp"), t4);
 
-    LoadBitmapTex(TEXT("bitmapy\\5.bmp"), t2);
+    LoadBitmapTex(TEXT("bitmapy\\5.bmp"), t5);
 
-    LoadBitmapTex(TEXT("bitmapy\\6.bmp"), t3);
+    LoadBitmapTex(TEXT("bitmapy\\6.bmp"), t6);
+
+    LoadBitmapTex(TEXT("bitmapy\\trojkat.bmp"), tr);
 
 
     glEnable(GL_DEPTH_TEST);
@@ -248,8 +335,8 @@ int main(int argc, char* args[])
     glLoadIdentity();
     //inicjalizacja koloru tla na czerny (RGB 0-1,0-1,0-1), kanał alpha
     glClearColor(0.f, 0.f, 0.f, 1.f);
-    
 
+    printf("----- KLAWISZE ----\n\n t  -  zmiana sceny (kostka / trojkat)\n x, y, z - obrot kostki");
 
     glutDisplayFunc(funkcjaRenderujaca);
     //glutIdleFunc(display);
